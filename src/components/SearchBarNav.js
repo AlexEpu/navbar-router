8193260eaf3ad4a6bcd9d1a6a6e22cbcaa7c36fc
AlexEpu <65967586+AlexEpu@users.noreply.github.com>
2020-09-10 04:17:58 +0300

    aaa

--- src/components/SearchBarNav.js
+++ src/components/SearchBarNav.js
@@ -4,7 +4,6 @@ import "./SearchBarNav.css";
 import { Button } from "./Button";
 import searchIcon from "./search-icon.png";
 
-
 class SearchBarNav extends Component {
   constructor() {
     super();
@@ -23,53 +22,21 @@ class SearchBarNav extends Component {
   render() {
     let searchForm = this.state.showForm ? (
       <form className="menu__search-form" method="POST">
-        <input
-          className="menu__search-input"
-          placeholder="quick search...."
-        />
+        <input className="menu__search-input" placeholder="quick search...." />
       </form>
     ) : (
       ""
     );
 
-    // let linksMarkup = this.props.links.map((link, index) => {
-    //   let linkMarkup = link.active ? (
-    //     <a className="menu__link menu__link--active" href={link.link}>
-    //       {link.label}
-    //     </a>
-    //   ) : (
-    //     <a className="menu__link" href={link.link}>
-    //       {link.label}
-    //     </a>
-    //   );
-
-    //   return (
-    //     <li key={index} className="menu__list-item">
-    //       {linkMarkup}
-    //     </li>
-    //   );
-    // });
-
     return (
-      
       <div className="menu__right">
-        
         <button
-                   
-          
-                
-            onClick={this.showForm.bind(this)}
-            style={{
-            
-              backgroundImage: "url(" + searchIcon + ")",
-            }}
-            className="menu__search-button"
-          
-          ></button>
-        
-          {searchForm}
-        </div>
-      
+          onClick={this.showForm.bind(this)}
+          style={{ backgroundImage: "url(" + searchIcon + ")" }}
+          className="menu__search-button"
+        ></button>
+        {searchForm}
+      </div>
     );
   }
 }
